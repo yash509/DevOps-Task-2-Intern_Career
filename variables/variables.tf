fe71dcd8f70bd229fe66a375af7450facb3489a9
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-05-07 08:25:55 +0530

    Update variables.tf

--- variables/variables.tf
+++ variables/variables.tf
@@ -1,35 +1,35 @@
 variable "region" {
-  description = "this gives region description"
+  description = "region description"
   type = string
 }
 
 variable "vpc_name" {
-  description = "this gives vpc description"
+  description = "vpc description"
   type = string
 }
 
 variable "vpc_cidr" {
-  description = "this gives vpc cidr description"
+  description = "vpc cidr range description"
   type = string
 }
 
 variable "subnetA_name" {
-  description = "this gives Subnet 1 description"
+  description = "Subnet 1 description"
   type = string
 }
 
 variable "subnetA_cidr" {
-  description = "this gives subnet 1 cidr description"
+  description = "subnet 1 cidr range description"
   type = string
 }
 
 variable "subnetB_name" {
-  description = "this gives subnet 2 description"
+  description = "subnet 2 description"
   type = string
 }
 
 variable "subnetB_cidr" {
-  description = "this gives subnet 2 description"
+  description = "subnet 2 cidr range description"
   type = string
 }
 
